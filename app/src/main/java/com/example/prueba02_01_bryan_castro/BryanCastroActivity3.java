d7ac9c018df43559efc806f3af806653bcc98951
BryanC714 <165086565+BryanC714@users.noreply.github.com>
2025-06-11 18:30:54 -0500

    Bryan Castro 18:30

---
+++ app/src/main/java/com/example/prueba02_01_bryan_castro/BryanCastroActivity3.java
@@ -0,0 +1,24 @@
+package com.example.prueba02_01_bryan_castro;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BryanCastroActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_bryan_castro3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
