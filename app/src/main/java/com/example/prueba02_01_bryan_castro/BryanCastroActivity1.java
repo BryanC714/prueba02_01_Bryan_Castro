4420de8026e063d769a0085a2cbbe6027e64c106
BryanC714 <165086565+BryanC714@users.noreply.github.com>
2025-06-11 19:00:11 -0500

    Bryan Castro 19:00

--- app/src/main/java/com/example/prueba02_01_bryan_castro/BryanCastroActivity1.java
+++ app/src/main/java/com/example/prueba02_01_bryan_castro/BryanCastroActivity1.java
@@ -1,6 +1,10 @@
 package com.example.prueba02_01_bryan_castro;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class BryanCastroActivity1 extends AppCompatActivity {
-
+    private EditText editTextNombres;
+    private EditText editTextApellidos;
+    private EditText editTextDividendo;
+    private EditText editTextDivisor;
+    private EditText editTextParteEntera ;
+    private EditText editTextResiduo;
+    private EditText editTextNumeroInvertido ;
+    private Button buttonMostrar;
+    private Button buttonSiguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +32,22 @@ public class BryanCastroActivity1 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        editTextNombres=findViewById(R.id.editText_Nombres);
+        editTextApellidos=findViewById(R.id.editText_Apellidos);
+        editTextDividendo=findViewById(R.id.editText_Dividendo);
+        editTextDivisor=findViewById(R.id.editTextDivisor);
+        editTextParteEntera=findViewById(R.id.editTextParteEntera);
+        editTextResiduo=findViewById(R.id.editTextResiduo);
+        editTextNumeroInvertido=findViewById(R.id.editTextNumeroI);
+        buttonMostrar=findViewById(R.id.buttonMostrar);
+        buttonSiguiente=findViewById(R.id.buttonSp1);
+
+        buttonMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),BryanCastroActivity2.class);
+            }
+        });
+
     }
 }
